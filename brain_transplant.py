25df3c2d3a68bc457ca68754d690d29777cde817
MiDe-S <63002269+MiDe-S@users.noreply.github.com>
2021-04-02 14:52:00 -0400

    Transplant GUI basically done

--- brain_transplant.py
+++ brain_transplant.py
@@ -39,6 +39,8 @@ def main():
     bin_name_key = '_bin-name_'
     success_text_key = '_transplant-success_'
     randomize_sn_key = '_randomize-sn_'
+    folder_location_key = '_selected-folder_'
+    browsed_key = '_browse-submission_'
 
     # gets current key_directory
     directory = os.path.dirname(os.path.realpath(__file__))
@@ -56,15 +58,28 @@ def main():
     characters = list(char_dict.keys())
     characters.insert(0, "Random")
 
-    layout = [[sg.Text('My one-shot window.')],
-              [sg.Input(key=submitted_key, enable_events=True, visible=False)],
-              [sg.Listbox(found_bins, sg.LISTBOX_SELECT_MODE_SINGLE, size=(30, 10), key=bin_name_key),
+    transplant_layout = [[sg.FolderBrowse(target=browsed_key, key=folder_location_key, enable_events=True), sg.Text("Currently looking at:"), sg.Text(directory, key="test", auto_size_text=True)],
+              [sg.Input(key=submitted_key, enable_events=True, visible=False), sg.Input(key=browsed_key, enable_events=True, visible=False)],
+              [sg.Listbox(found_bins, sg.LISTBOX_SELECT_MODE_SINGLE, size=(40, 10), key=bin_name_key), sg.VerticalSeparator(),
                sg.Listbox(characters, sg.LISTBOX_SELECT_MODE_SINGLE, size=(30, 10), key=character_key)],
-              [sg.Checkbox("Randomize Serial Number", key=randomize_sn_key, enable_events=True, default=True),
-               sg.FileSaveAs("Transplant", target=submitted_key, key=save_location_key, file_types=(('Bin Files', '*.bin'),), default_extension=".bin", initial_folder=directory)],
+              [sg.FileSaveAs("Transplant", target=submitted_key, key=save_location_key, file_types=(('Bin Files', '*.bin'),), default_extension=".bin", initial_folder=directory),
+               sg.Checkbox("Randomize Serial Number", key=randomize_sn_key, enable_events=True, default=True)],
               [sg.Text(key=success_text_key, size=(10, 1), visible=False)]]
 
-    window = sg.Window('MiDes Brain Transplant Service - Version {}'.format(version_number), layout)
+    serial_swapper_layout = [[sg.Text("Lasaga wins amiibo")]]
+
+    character_editor_layout = [[sg.Text("who knows")]]
+
+    about_layout = [[sg.Text("Version Number {}".format(version_number))],
+                    [sg.Text("If you encounter issues raise a issue on github or dm MiDe#9934 on discord")],
+                    [sg.Text("Shoutouts to the amiibo homies at USAC: https://discord.gg/2SEqk9p", tooltip="I'm too lazy to make this an actual link for now")]]
+
+    tabs = [[sg.TabGroup([[sg.Tab("Transplant", transplant_layout),
+             sg.Tab("Serial Number Swapper", serial_swapper_layout),
+             sg.Tab("Characters.txt Editor", character_editor_layout),
+             sg.Tab("About", about_layout)]])]]
+
+    window = sg.Window('MiDes Brain Transplant Service'.format(version_number), tabs)
 
     w = True
     while w:
@@ -85,6 +100,19 @@ def main():
                 window[success_text_key].set_size((len(success_message), 1))
                 # refresh window to show updates
                 window.refresh()
+        if event == browsed_key:
+            new_directory = values[folder_location_key]
+            # changes bin list when new folder is picked
+            new_found_bins = []
+            for bin_file in os.listdir(new_directory):
+                if bin_file[-3:] == "bin":
+                    if bin_file != 'unfixed-info.bin' and bin_file != 'locked-secret.bin':
+                        new_found_bins.append(bin_file[:-4])
+            window[bin_name_key].update(new_found_bins)
+            #window["test"].set_size((len(new_directory), 1))
+            window["test"].update(new_directory)
+            window.refresh()
+
         if event == sg.WIN_CLOSED:
             break
 
